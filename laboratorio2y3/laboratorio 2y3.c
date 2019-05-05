35c428bba3b0cc9bd22a3592039c12eb23428707
arayajuan1 <50241315+arayajuan1@users.noreply.github.com>
2019-05-05 17:56:03 -0400

    Add files via upload

---
+++ laboratorio2y3/laboratorio 2y3.c
@@ -0,0 +1,21 @@
+#include "tiemposdebusqueda.h"
+
+////////////////////////////
+void main()
+{
+	int tamano_inicial_lista=tamanoinicial;
+	double tiemp_de_busqueda;
+	
+	int i=0,num=0,op=0;
+	printf("ingrese opcion que desea realizar \n");
+	printf("1: tomar tiempo busqueda secuencial \n");
+	printf("2:tomar tiempo busqueda binaria \n");
+	scanf("%d",&op);
+	switch(op)
+	{
+		case 1: busqueda_secuencial(tamano_inicial_lista,tiemp_de_busqueda);break;
+		case 2: busqueda_binaria(tamano_inicial_lista,tiemp_de_busqueda);break;
+		default: printf("ingrese opcion valida \n");break;
+	 } 
+	
+}
